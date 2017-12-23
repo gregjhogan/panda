a88aec2123073bddc6b6c841e3516309278533cc
Greg Hogan <gregjhogan@gmail.com>
2017-12-22 20:55:33 -0600

    updated dump script to show forwarded traffic

--- examples/dump.py
+++ examples/dump.py
@@ -5,21 +5,15 @@ from panda import Panda
 
 if __name__ == "__main__":
   panda = Panda()
-  # panda.set_safety_mode(Panda.SAFETY_ELM327)
+  panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
 
-  # panda.can_clear(0)
-  # time.sleep(1.0)
-  for z in range(0,3):
-    addr = 0x18DBEFF1
-    msg = "\x02\x3E\x00".ljust(8, "\x00")
-    #print "S:", format(addr,'x'), msg.encode("hex")
-    #panda.can_send(addr, msg, 0)
-    while (1):
-      msgs = panda.can_recv()
-      for ids, ts, dat, bus in msgs:
-        #if bus == 0 and ids == ((addr >> 16 << 16) | (addr << 8 & 0xFFFF) | (addr >> 8 & 0xFF)):
-        if bus == 0 and ids > 0xFFFF:
-          print "R:", format(ids,'x'), ("".join("%02x" % i for i in dat))
-      time.sleep(0.01)
-
-  print "done"
+  while (1):
+    msgs = panda.can_recv()
+    for ids, ts, dat, bus in msgs:
+      #if bus == 0 and ids == ((addr >> 16 << 16) | (addr << 8 & 0xFFFF) | (addr >> 8 & 0xFF)):
+      #if bus == 0 and ids > 0xFFFF
+      #if ids == 0xE4 or ids == 0x1DF or ids == 0x1EF or ids == 0x30C or ids == 0x33D or ids == 0x39F:
+      if bus == 129 or bus == 130:
+        #print "R:", format(ids,'x'), ("".join("%02x" % i for i in dat))
+        print "R:", bus, "-", format(ids,'x')
+    #time.sleep(0.001)
