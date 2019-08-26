8db08c4fdb129b44c38dfce99bd62fbb48eba6b2
Greg Hogan <gregjhogan@gmail.com>
2019-08-25 18:53:58 -0700

    log searching scripts

--- examples/can_count_search.py
+++ examples/can_count_search.py
@@ -6,17 +6,17 @@ import sys
 
 class Message():
   """Details about a specific message ID."""
-  def __init__(self, message_id):
+  def __init__(self, message_id, min_count):
     self.message_id = message_id
-    self.matches = [0] * 8 # 1 = always matches desired value
-    self.min = [0] * 8
-    self.max = [0] * 8
+    self.count = 0
+    self.matches = [0] * 8 # count of matches of desired value
+    self.min_count = min_count
 
   def printMatches(self):
     """Prints bits that transition from always zero to always 1 and vice versa."""
     for i in xrange(len(self.matches)):
-      if self.matches[i]:
-        print 'id %s matches at byte %d (%d-%d)' % (self.message_id, i, self.min[i], self.max[i])
+      if self.matches[i] >= self.min_count:
+        print 'id %s matches at byte %d (%d of %d)' % (self.message_id, i, self.matches[i], self.count)
 
 class Info():
   """A collection of Messages."""
@@ -24,7 +24,7 @@ class Info():
   def __init__(self):
     self.messages = {}  # keyed by MessageID
 
-  def load(self, filename, value, tolerance, start, end):
+  def load(self, filename, value, min_count, start, end):
     """Given a CSV file, adds information about message IDs and their values."""
     with open(filename, 'rb') as input:
       reader = csv.reader(input)
@@ -46,35 +46,27 @@ class Info():
           data = row[3][2:]  # remove leading '0x'
         else:
           data = row[3]
-        new_message = False
         if message_id not in self.messages:
-          self.messages[message_id] = Message(message_id)
-          new_message = True
+          self.messages[message_id] = Message(message_id, min_count)
         message = self.messages[message_id]
+        message.count += 1
         bytes = bytearray.fromhex(data)
         for i in xrange(len(bytes)):
           byte = int(bytes[i])
-          if new_message:
-            message.matches[i] = abs(byte - value) <= tolerance
-            message.min[i] = byte
-            message.max[i] = byte
-          else:
-            if message.min[i] > byte: message.min[i] = byte
-            if message.max[i] < byte: message.max[i] = byte
-            if message.matches[i] == 1 and abs(byte - value) > tolerance:
-              message.matches[i] = 0
+          if byte == value:
+            message.matches[i] += 1
 
-def PrintMatchingValues(log_file, value, tolerance, time_range):
+def PrintMatchingValues(log_file, value, min_count, time_range):
   value = int(value)
-  tolerance = int(tolerance)
+  min_count = int(min_count)
   start, end = map(float, time_range.split('-'))
   info = Info()
-  info.load(log_file, value, tolerance, start, end)
+  info.load(log_file, value, min_count, start, end)
   for message_id in sorted(info.messages):
     info.messages[message_id].printMatches()
 
 if __name__ == "__main__":
   if len(sys.argv) < 5:
-    print 'Usage:\n%s log.csv <value> <tolerance> <start>-<end>' % sys.argv[0]
+    print 'Usage:\n%s log.csv <value> <min-count> <start>-<end>' % sys.argv[0]
     sys.exit(0)
   PrintMatchingValues(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
--- examples/can_list_search.py
+++ examples/can_list_search.py
@@ -6,17 +6,17 @@ import sys
 
 class Message():
   """Details about a specific message ID."""
-  def __init__(self, message_id):
+  def __init__(self, message_id, min_unique_count):
     self.message_id = message_id
-    self.matches = [0] * 8 # 1 = always matches desired value
-    self.min = [0] * 8
-    self.max = [0] * 8
+    self.min_unique_count = min_unique_count
+    self.matches = [0] * 8 # 1 = contains all desired values
+    self.unique = [set() for i in range(8)] # contains unique values matched
 
   def printMatches(self):
     """Prints bits that transition from always zero to always 1 and vice versa."""
     for i in xrange(len(self.matches)):
-      if self.matches[i]:
-        print 'id %s matches at byte %d (%d-%d)' % (self.message_id, i, self.min[i], self.max[i])
+      if self.matches[i] and len(self.unique[i]) >= self.min_unique_count:
+        print('id {} matches at byte {} {}'.format(self.message_id, i, self.unique[i]))
 
 class Info():
   """A collection of Messages."""
@@ -24,7 +24,7 @@ class Info():
   def __init__(self):
     self.messages = {}  # keyed by MessageID
 
-  def load(self, filename, value, tolerance, start, end):
+  def load(self, filename, values, min_unique_count, start, end):
     """Given a CSV file, adds information about message IDs and their values."""
     with open(filename, 'rb') as input:
       reader = csv.reader(input)
@@ -48,33 +48,29 @@ class Info():
           data = row[3]
         new_message = False
         if message_id not in self.messages:
-          self.messages[message_id] = Message(message_id)
+          self.messages[message_id] = Message(message_id, min_unique_count)
           new_message = True
         message = self.messages[message_id]
         bytes = bytearray.fromhex(data)
         for i in xrange(len(bytes)):
           byte = int(bytes[i])
           if new_message:
-            message.matches[i] = abs(byte - value) <= tolerance
-            message.min[i] = byte
-            message.max[i] = byte
-          else:
-            if message.min[i] > byte: message.min[i] = byte
-            if message.max[i] < byte: message.max[i] = byte
-            if message.matches[i] == 1 and abs(byte - value) > tolerance:
+            message.matches[i] = byte in values
+          elif message.matches[i] == 1 and byte not in values:
               message.matches[i] = 0
+          message.unique[i].add(byte)
 
-def PrintMatchingValues(log_file, value, tolerance, time_range):
-  value = int(value)
-  tolerance = int(tolerance)
+def PrintMatchingValues(log_file, values, min_unique_count, time_range):
+  values = map(int, values.split(','))
+  min_unique_count = int(min_unique_count)
   start, end = map(float, time_range.split('-'))
   info = Info()
-  info.load(log_file, value, tolerance, start, end)
+  info.load(log_file, values, min_unique_count, start, end)
   for message_id in sorted(info.messages):
     info.messages[message_id].printMatches()
 
 if __name__ == "__main__":
   if len(sys.argv) < 5:
-    print 'Usage:\n%s log.csv <value> <tolerance> <start>-<end>' % sys.argv[0]
+    print 'Usage:\n%s log.csv <value1,value2,value3> <min-unique-match-count> <start>-<end>' % sys.argv[0]
     sys.exit(0)
   PrintMatchingValues(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
