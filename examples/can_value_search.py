eaa9a808640d3d055555ba68918c79709e4e7a31
Greg Hogan <gregjhogan@gmail.com>
2019-08-25 17:25:38 -0700

    update can_value_search.py to support range

--- examples/can_value_search.py
+++ examples/can_value_search.py
@@ -9,12 +9,14 @@ class Message():
   def __init__(self, message_id):
     self.message_id = message_id
     self.matches = [0] * 8 # 1 = always matches desired value
+    self.min = [0] * 8
+    self.max = [0] * 8
 
   def printMatches(self):
     """Prints bits that transition from always zero to always 1 and vice versa."""
     for i in xrange(len(self.matches)):
       if self.matches[i]:
-        print 'id %s matches at byte %d' % (self.message_id, i)
+        print 'id %s matches at byte %d (%d-%d)' % (self.message_id, i, self.min[i], self.max[i])
 
 class Info():
   """A collection of Messages."""
@@ -22,7 +24,7 @@ class Info():
   def __init__(self):
     self.messages = {}  # keyed by MessageID
 
-  def load(self, filename, value, start, end):
+  def load(self, filename, value, tolerance, start, end):
     """Given a CSV file, adds information about message IDs and their values."""
     with open(filename, 'rb') as input:
       reader = csv.reader(input)
@@ -53,20 +55,26 @@ class Info():
         for i in xrange(len(bytes)):
           byte = int(bytes[i])
           if new_message:
-            message.matches[i] = abs(byte - value) == 0 # <= 10
-          elif message.matches[i] == 1 and abs(byte - value) != 0: # >= 10:
-            message.matches[i] = 0
+            message.matches[i] = abs(byte - value) <= tolerance
+            message.min[i] = byte
+            message.max[i] = byte
+          else:
+            if message.min[i] > byte: message.min[i] = byte
+            if message.max[i] < byte: message.max[i] = byte
+            if message.matches[i] == 1 and abs(byte - value) > tolerance:
+              message.matches[i] = 0
 
-def PrintMatchingValues(log_file, value, range):
+def PrintMatchingValues(log_file, value, tolerance, time_range):
   value = int(value)
-  start, end = map(float, range.split('-'))
+  tolerance = int(tolerance)
+  start, end = map(float, time_range.split('-'))
   info = Info()
-  info.load(log_file, value, start, end)
+  info.load(log_file, value, tolerance, start, end)
   for message_id in sorted(info.messages):
     info.messages[message_id].printMatches()
 
 if __name__ == "__main__":
   if len(sys.argv) < 4:
-    print 'Usage:\n%s log.csv <value> <start>-<end>' % sys.argv[0]
+    print 'Usage:\n%s log.csv <value> <tolerance> <start>-<end>' % sys.argv[0]
     sys.exit(0)
-  PrintMatchingValues(sys.argv[1], sys.argv[2], sys.argv[3])
+  PrintMatchingValues(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
